cd7282a766ead70e25b810d4a259b3cc47f6b091
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-02-22 10:53:18 -0400

    feat: Implemneta estrutura de agendamentos

--- src/presentation/pages/Schedules/index.tsx
+++ src/presentation/pages/Schedules/index.tsx
@@ -1,2 +1,22 @@
-const a = <></>
-export { a }
+import { Scheduling } from "../../../domain/entity/schedules/types"
+
+interface SchedulesProps {
+    schedules: Scheduling[]
+}
+
+function Schedules({ schedules }: SchedulesProps) {
+    if (!schedules) return (
+        <></>
+    )
+
+    return (
+        <div>
+            <div>{schedules[0] && schedules[0].date}</div>
+            <div>{schedules[0] && schedules[0].hour}</div>
+            <div>{schedules[0] && schedules[0].service}</div>
+            <div>{schedules[0] && schedules[0].name}</div>
+        </div>
+    )
+}
+
+export { Schedules }
